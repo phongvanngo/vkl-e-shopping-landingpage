551da2e7eaf41c258069958214ab089d6298af88
phongvanngo <19520834@gm.uit.edu.vn>
2021-06-05 17:57:09 +0700

    setup redux product category

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,28 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loadingReducer from "./loadingSlice";
-import adminAuthReducer from "./adminAuthSlice";
-import userAuthReducer from "./userAuthSlice";
-import commonReducer from "./commonSlice";
 import dialogReducer from "./dialogSlice";
-import theaterReducer from "./theaterSlice";
-import movieReducer from "./movieSlice";
-import cumRapReducer from "./cumRapSlice";
-import phongChieuReducer from "./phongChieuSlice";
-import movieTypeReducer from "./movieTypeSlice";
-import showTimeReducer from "./showTimeSlice";
+import categoryReducer from "./categorySlice";
 
 export const store = configureStore({
   reducer: {
     loading: loadingReducer,
-    adminAuth: adminAuthReducer,
-    userAuth: userAuthReducer,
-    common: commonReducer,
+    category: categoryReducer,
     dialog: dialogReducer,
-    theater: theaterReducer,
-    movie: movieReducer,
-    cumRap: cumRapReducer,
-    phongChieu: phongChieuReducer,
-    movieType: movieTypeReducer,
-    showTime: showTimeReducer,
   },
 });
