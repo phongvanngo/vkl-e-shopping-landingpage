bef64db3f0dff7b0eafeed4191dc203b1e654172
test <ngomandat234@gmail.com>
2021-12-20 17:39:41 +0700

    login register completed

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,193 +1,208 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import Header from "../layout/header";
-import Footer from "../layout/footer";
+import React, { Component, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import Header from '../layout/header'
+import Footer from '../layout/footer'
+import axiosClient from '../app/axiosClient'
 
-class Login extends Component {
-  render() {
-    return (
-      <>
-        <Header />
+function Login() {
+  const history = useHistory()
+  var [username, setUserName] = useState()
+  var [password, setPassWord] = useState()
+  function handleLogin() {
+    var user = { username, password }
+    axiosClient
+      .post('/api/auth/login', user)
+      .then((res) => localStorage.setItem('user', JSON.stringify(res)))
+    history.push('/')
+  }
+  return (
+    <>
+      <Header />
 
-        <div className="main-container col1-layout wow bounceInUp animated animated">
-          <div className="main">
-            <div className="account-login container">
-              {/* <!--page-title--> */}
+      <div className='main-container col1-layout wow bounceInUp animated animated'>
+        <div className='main'>
+          <div className='account-login container'>
+            {/* <!--page-title--> */}
 
-              <form action="" method="post" id="login-form">
-                <input name="form_key" type="hidden" value="EPYwQxF6xoWcjLUr" />
-                <fieldset className="col2-set">
-                  <div className="col-1 new-users">
-                    <strong>New Customers</strong>
-                    <div className="content">
-                      <p>
-                        By creating an account with our store, you will be able
-                        to move through the checkout process faster, store
-                        multiple shipping addresses, view and track your orders
-                        in your account and more.
-                      </p>
-                      <div className="buttons-set">
-                        <button
-                          type="button"
-                          title="Create an Account"
-                          className="button create-account"
-                          onClick=""
-                        >
+            <div id='login-form'>
+              <input name='form_key' type='hidden' value='EPYwQxF6xoWcjLUr' />
+              <fieldset className='col2-set'>
+                <div className='col-1 new-users'>
+                  <strong>New Customers</strong>
+                  <div className='content'>
+                    <p>
+                      By creating an account with our store, you will be able to
+                      move through the checkout process faster, store multiple
+                      shipping addresses, view and track your orders in your
+                      account and more.
+                    </p>
+                    <div className='buttons-set'>
+                      <button
+                        type='button'
+                        title='Create an Account'
+                        className='button create-account'
+                        onClick=''
+                      >
+                        <span>
                           <span>
-                            <span>Create an Account</span>
+                            <Link to='/register'>Create an account</Link>
                           </span>
-                        </button>
-                      </div>
+                        </span>
+                      </button>
                     </div>
                   </div>
-                  <div className="col-2 registered-users">
-                    <strong>Registered Customers</strong>
-                    <div className="content">
-                      <p>If you have an account with us, please log in.</p>
-                      <ul className="form-list">
-                        <li>
-                          <label for="email">
-                            Email Address<em className="required">*</em>
-                          </label>
-                          <div className="input-box">
-                            <input
-                              type="text"
-                              name="login[username]"
-                              value=""
-                              id="email"
-                              className="input-text required-entry validate-email"
-                              title="Email Address"
-                            />
-                          </div>
-                        </li>
-                        <li>
-                          <label for="pass">
-                            Password<em className="required">*</em>
-                          </label>
-                          <div className="input-box">
-                            <input
-                              type="password"
-                              name="login[password]"
-                              className="input-text required-entry validate-password"
-                              id="pass"
-                              title="Password"
-                            />
-                          </div>
-                        </li>
-                      </ul>
-                      <div className="remember-me-popup">
-                        <div className="remember-me-popup-head">
-                          <h3 id="text2">What's this?</h3>
-                          <Link
-                            to={" "}
-                            className="remember-me-popup-close"
-                            onClick="showDiv()"
-                            title="Close"
-                          >
-                            Close
-                          </Link>
+                </div>
+                <div className='col-2 registered-users'>
+                  <strong>Registered Customers</strong>
+                  <div className='content'>
+                    <p>If you have an account with us, please log in.</p>
+                    <ul className='form-list'>
+                      <li>
+                        <label for='email'>
+                          Email Address<em className='required'>*</em>
+                        </label>
+                        <div className='input-box'>
+                          <input
+                            type='text'
+                            name='login[username]'
+                            id='email'
+                            className='input-text required-entry validate-email'
+                            title='Email Address'
+                            value={username}
+                            onChange={(e) => setUserName(e.target.value)}
+                          />
                         </div>
-                        <div className="remember-me-popup-body">
-                          <p id="text1">
-                            Checking "Remember Me" will let you access your
-                            shopping cart on this computer when you are logged
-                            out
-                          </p>
-                          <div className="remember-me-popup-close-button a-right">
-                            <Link
-                              to={" "}
-                              className="remember-me-popup-close button"
-                              title="Close"
-                              onClick="
-            showDiv()"
-                            >
-                              <span>Close</span>
-                            </Link>
-                          </div>
+                      </li>
+                      <li>
+                        <label for='pass'>
+                          Password<em className='required'>*</em>
+                        </label>
+                        <div className='input-box'>
+                          <input
+                            type='password'
+                            name='login[password]'
+                            className='input-text required-entry validate-password'
+                            id='pass'
+                            title='Password'
+                            value={password}
+                            onChange={(e) => setPassWord(e.target.value)}
+                          />
                         </div>
-                      </div>
-
-                      <p className="required">* Required Fields</p>
-
-                      <div className="buttons-set">
-                        <button
-                          type="submit"
-                          className="button login"
-                          title="Login"
-                          name="send"
-                          id="send2"
+                      </li>
+                    </ul>
+                    <div className='remember-me-popup'>
+                      <div className='remember-me-popup-head'>
+                        <h3 id='text2'>What's this?</h3>
+                        <Link
+                          to={' '}
+                          className='remember-me-popup-close'
+                          onClick='showDiv()'
+                          title='Close'
                         >
-                          <span>Login</span>
-                        </button>
-
-                        <Link to={" "} className="forgot-word">
-                          Forgot Your Password?
+                          Close
                         </Link>
                       </div>
-                      {/* <!--buttons-set--> */}
+                      <div className='remember-me-popup-body'>
+                        <p id='text1'>
+                          Checking "Remember Me" will let you access your
+                          shopping cart on this computer when you are logged out
+                        </p>
+                        <div className='remember-me-popup-close-button a-right'>
+                          <Link
+                            to={' '}
+                            className='remember-me-popup-close button'
+                            title='Close'
+                            onClick='
+            showDiv()'
+                          >
+                            <span>Close</span>
+                          </Link>
+                        </div>
+                      </div>
                     </div>
-                    {/* <!--content--> */}
+
+                    <p className='required'>* Required Fields</p>
+
+                    <div className='buttons-set'>
+                      <button
+                        type='submit'
+                        className='button login'
+                        title='Login'
+                        name='send'
+                        id='send2'
+                        onClick={handleLogin}
+                      >
+                        <span>Login</span>
+                      </button>
+
+                      <Link to={' '} className='forgot-word'>
+                        Forgot Your Password?
+                      </Link>
+                    </div>
+                    {/* <!--buttons-set--> */}
                   </div>
-                  {/* <!--col-2 registered-users--> */}
-                </fieldset>
-                {/* <!--col2-set--> */}
-              </form>
+                  {/* <!--content--> */}
+                </div>
+                {/* <!--col-2 registered-users--> */}
+              </fieldset>
+              {/* <!--col2-set--> */}
             </div>
-            {/* <!--account-login--> */}
           </div>
-          {/* <!--main-container--> */}
+          {/* <!--account-login--> */}
         </div>
-        {/* <!--col1-layout--> */}
+        {/* <!--main-container--> */}
+      </div>
+      {/* <!--col1-layout--> */}
 
-        <div className="container">
-          <div className="row our-features-box">
-            <ul>
-              <li>
-                <div className="feature-box">
-                  <div className="icon-truck"></div>
-                  <div className="content">FREE SHIPPING on order over $99</div>
+      <div className='container'>
+        <div className='row our-features-box'>
+          <ul>
+            <li>
+              <div className='feature-box'>
+                <div className='icon-truck'></div>
+                <div className='content'>FREE SHIPPING on order over $99</div>
+              </div>
+            </li>
+            <li>
+              <div className='feature-box'>
+                <div className='icon-support'></div>
+                <div className='content'>
+                  Have a question?
+                  <br />
+                  +1 800 789 0000
                 </div>
-              </li>
-              <li>
-                <div className="feature-box">
-                  <div className="icon-support"></div>
-                  <div className="content">
-                    Have a question?
-                    <br />
-                    +1 800 789 0000
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div className="feature-box">
-                  <div className="icon-money"></div>
-                  <div className="content">100% Money Back Guarantee</div>
-                </div>
-              </li>
-              <li>
-                <div className="feature-box">
-                  <div className="icon-return"></div>
-                  <div className="content">30 days return Service</div>
-                </div>
-              </li>
-              <li className="last">
-                <div className="feature-box">
-                  {" "}
-                  <Link to={" "}>
-                    <i className="fa fa-apple"></i> download
-                  </Link>{" "}
-                  <Link to={" "}>
-                    <i className="fa fa-android"></i> download
-                  </Link>{" "}
-                </div>
-              </li>
-            </ul>
-          </div>
+              </div>
+            </li>
+            <li>
+              <div className='feature-box'>
+                <div className='icon-money'></div>
+                <div className='content'>100% Money Back Guarantee</div>
+              </div>
+            </li>
+            <li>
+              <div className='feature-box'>
+                <div className='icon-return'></div>
+                <div className='content'>30 days return Service</div>
+              </div>
+            </li>
+            <li className='last'>
+              <div className='feature-box'>
+                {' '}
+                <Link to={' '}>
+                  <i className='fa fa-apple'></i> download
+                </Link>{' '}
+                <Link to={' '}>
+                  <i className='fa fa-android'></i> download
+                </Link>{' '}
+              </div>
+            </li>
+          </ul>
         </div>
+      </div>
 
-        <Footer />
-      </>
-    );
-  }
+      <Footer />
+    </>
+  )
 }
-export default Login;
+
+export default Login
